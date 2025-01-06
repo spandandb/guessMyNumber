d33c3e09b44d8552692aba5fd60f9888c2f0e2b3
spandandb <spandandb94@gmail.com>
2025-01-06 22:28:34 +0530

    enter button now executes the check button functionality as well

--- script.js
+++ script.js
@@ -14,7 +14,48 @@ const msg = document.querySelector(".message");
 const guess = document.querySelector(".guess");
 
 // adding Event Listener when user enters the value to the input box
-document.querySelector(".check").addEventListener("click", function () {
+document.querySelector(".check").addEventListener("click", playGame);
+
+// user can now press enter instead of clicking and still the check btn logic will be executed
+document.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    playGame();
+  }
+});
+
+// implementating functionality of Again button
+document.querySelector(".again").addEventListener("click", function () {
+  // generating new random number
+  secretNumber = Math.trunc(Math.random() * 30) + 1;
+
+  // mresetting the no. of attempts and changing the value in the DOM
+  score = 10;
+  document.querySelector(".score").textContent = 10;
+
+  const guess = document.querySelector(".guess");
+  const check = document.querySelector(".check");
+
+  // removing the attribute as the btn should no longer be disabled
+  guess.removeAttribute("disabled");
+  check.removeAttribute("disabled");
+
+  // removing the css for disabled state
+  guess.classList.remove("disabled");
+  check.classList.remove("disabled");
+
+  // taking back the html and css to the initial state
+  document.body.style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+
+  msg.textContent = "Start guessing...";
+
+  document.querySelector("h1").textContent = "Guess My Number!";
+  document.querySelector(".number").textContent = "?";
+
+  guess.value = "";
+});
+
+function playGame() {
   const userGuess = Number(document.querySelector(".guess").value);
   const guess = document.querySelector(".guess");
 
@@ -52,39 +93,7 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".number").textContent = secretNumber;
     }
   }
-});
-
-// implementating functionality of Again button
-document.querySelector(".again").addEventListener("click", function () {
-  // generating new random number
-  secretNumber = Math.trunc(Math.random() * 30) + 1;
-
-  // mresetting the no. of attempts and changing the value in the DOM
-  score = 10;
-  document.querySelector(".score").textContent = 10;
-
-  const guess = document.querySelector(".guess");
-  const check = document.querySelector(".check");
-
-  // removing the attribute as the btn should no longer be disabled
-  guess.removeAttribute("disabled");
-  check.removeAttribute("disabled");
-
-  // removing the css for disabled state
-  guess.classList.remove("disabled");
-  check.classList.remove("disabled");
-
-  // taking back the html and css to the initial state
-  document.body.style.backgroundColor = "#222";
-  document.querySelector(".number").style.width = "15rem";
-
-  msg.textContent = "Start guessing...";
-
-  document.querySelector("h1").textContent = "Guess My Number!";
-  document.querySelector(".number").textContent = "?";
-
-  guess.value = "";
-});
+}
 
 // disable input field and buttons when the game finishes (either user wins or the number of attempts expires)
 function disableUserInput() {
